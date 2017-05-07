44c6c0ea3a4166f50cf2ca7cba68f071c0c26336
sneezeman <499thz@gmail.com>
2017-05-07 14:24:04 +0300

    07/05
    2. Причесать UI
    4. normalize speed at opening
    5. update cell number in entry
    6. choose extension

--- plot_graph_3.py
+++ plot_graph_3.py
@@ -1,4 +1,4 @@
-from Tkinter import Tk, Button, StringVar, DoubleVar, Label, Entry, Frame, Checkbutton, BooleanVar
+from Tkinter import Tk, Button, StringVar, DoubleVar, Label, Entry, Frame, Checkbutton, BooleanVar, OptionMenu
 from tkFileDialog import askopenfilename
 from time import strftime
 import tkMessageBox
@@ -18,6 +18,27 @@ speedNormalized = []
 number_in_seq = -1
 cmap = plt.get_cmap('hot')
 
+
+def f_open():
+	global data_file
+	global speedNormalized
+	speedNormalized = []
+	speed = []
+	data_file = askopenfilename()
+	data_path.set(data_file)
+	most_common_spikes(False)
+	with open(data_file, 'rb') as data:
+		lines = data.readlines()
+		for line in lines:
+			line_massive = line.split(';')
+			if line_massive[0] == 'None':
+				continue
+			else:
+				speed.append(float(line_massive[2]))
+	for i in range(len(speed)):
+		speedNormalized.append(speed[i]/float(max(speed)))
+
+
 def make_sure_path_exists(path):
     try:
         os.makedirs(path)
@@ -27,13 +48,7 @@ def make_sure_path_exists(path):
 
 def saveFigure(cellNumber):
 	make_sure_path_exists(dirName)
-	plt.savefig(dirName + '/' + cellNumber, bbox_inches='tight')
-
-def f_open():
-	global data_file
-	data_file = askopenfilename()
-	data_path.set(data_file)
-	
+	plt.savefig(dirName + '/' + cellNumber + extention.get(), bbox_inches='tight', dpi = 400)
 
 def most_common_spikes(from_button):
 	global counter
@@ -55,7 +70,6 @@ def most_common_spikes(from_button):
 
 def next_common(forward):
 	global number_in_seq
-	most_common_spikes(False)
 	if forward: 
 		if number_in_seq < len(counter)-1:
 			number_in_seq = number_in_seq + 1
@@ -67,9 +81,9 @@ def next_common(forward):
 		if number_in_seq != 0:
 			number_in_seq = number_in_seq - 1
 			main(counter[number_in_seq][0], False)
-	cell_number_plotted.set(counter[number_in_seq][0])
+	cellNumberChoosed.set(counter[number_in_seq][0])
 	if bool_save.get():
-		saveFigure(str(counter[number_in_seq][0]) + '.svg')
+		saveFigure(str(counter[number_in_seq][1]) + '-' + str(counter[number_in_seq][0]))
 	
 
 # plotting function: clear current, plot & redraw
@@ -83,6 +97,14 @@ def plot(theta, r, theta_s, r_s, speed):
     	ax.plot(theta, r, color = 'black', linewidth=0.7)
     ax.plot(theta_s, r_s, marker='2', markersize=10, color = 'green' ,linestyle = 'None')
     plt.gcf().canvas.draw()
+    firstPartOfName = 'clear plot'
+    if bool_save.get():
+    	for element in counter:
+    		if int(element[0]) == int(E1.get()):
+    			firstPartOfName = element[1]
+    	if firstPartOfName == 'clear plot':
+    		firstPartOfName = '0'
+    	saveFigure(str(firstPartOfName) + '-' + E1.get())
     toolbar.update()
 
 def plotAll(theta, r, theta_s, r_s, speed):
@@ -101,22 +123,20 @@ def plotAll(theta, r, theta_s, r_s, speed):
     toolbar.update()
 
 def mainShowAll():
-	most_common_spikes(False)
 	global firstTime
 	firstTime = True
 	for pair in counter:
 		main(pair[0], True)
 		firstTime = False
+	if bool_save.get():
+		saveFigure('All spikes')
 
 def main(spike_number, showAll):
-	cell_number_plotted.set('')
 	try:
 		int(spike_number)
 	except ValueError:
 		spike_number = 0
 
-	speed = []
-	
 	r = []
 	theta = []
 	r_s = []
@@ -130,17 +150,12 @@ def main(spike_number, showAll):
 			else:
 				r.append(line_massive[0])
 				theta.append(float(line_massive[1])*2*math.pi/360)
-				speed.append(float(line_massive[2]))
+
 			if not line_massive[0] == 'None' and line_massive[3] != 'None':
 				for k in line_massive[3].split(','):
 					if float(k) == float(spike_number):
 						r_s.append(line_massive[0])
 						theta_s.append(float(line_massive[1])*2*math.pi/360)
-
-	# Normalize speed to [0,1]
-	if not speedNormalized:
-		for i in range(len(speed)):
-			speedNormalized.append(speed[i]/float(max(speed)))
 
 	if showAll:
 		plotAll(theta, r, theta_s, r_s, speedNormalized)
@@ -149,13 +164,15 @@ def main(spike_number, showAll):
 
 
 def saveAll(trashhold):
-	plt.ioff()
+	try:
+		int(trashhold)
+	except ValueError:
+		trashhold = 1
 	most_common_spikes(False)
 	for i in range(len(counter)):
 		if counter[i][1] >= int(trashhold):
 			main(counter[i][0], False)
-			saveFigure(str(counter[i][0]) + '.svg')
-	plt.ion()
+			saveFigure(str(counter[i][1]) + '-' + str(counter[i][0]))
 
 def _quit():
     root.quit()     # stops mainloop
@@ -164,36 +181,45 @@ def _quit():
 # GUI
 root = Tk()
 data_path = StringVar()
-cell_number_plotted = StringVar()
+cellNumberChoosed = StringVar()
+trashhold = StringVar()
+trashhold.set(1)
 bool_save = BooleanVar()
 boolSpeed = BooleanVar()
 data_path.set('First of all, open a file!')
+extention = StringVar()
+extention.set(".png") # default value
+
 
 Button(text='Open', command=f_open).pack()
 Label(root, textvariable=data_path).pack()
-Button(text='Most common spikes', command= lambda: most_common_spikes(True)).pack()
+frame1 = Frame(root)
+frame1.pack()
+Button(frame1, text='Most common spikes list', command= lambda: most_common_spikes(True)).pack(side = 'left')
+Button(frame1, text = "Show all spikes", command = lambda: mainShowAll()).pack(side = 'left')
 frame2 = Frame(root)
 frame2.pack()
 Label(frame2, text="Cell number: ").pack(side='left')
-E1 = Entry(frame2, bd=5)
+E1 = Entry(frame2, textvariable = cellNumberChoosed, bd=5)
 E1.pack(side='left')
-Label(frame2, textvariable = cell_number_plotted).pack(side = 'left')
-frame1 = Frame(root)
-frame1.pack()
-Button(frame1, text="Prev common", command = lambda: next_common(False)).pack(side='left')
-Button(frame1, text="Plot!", command = lambda: main(E1.get(),False)).pack(side='left')
-Button(frame1, text="Next common", command = lambda: next_common(True)).pack(side='left')
-Checkbutton(frame1, text="Save?", variable = bool_save).pack(side='left')
-Checkbutton(frame1, text="Speed?", variable = boolSpeed).pack(side='left')
-Button(root, text = "Show all", command = lambda: mainShowAll()).pack()
+Button(frame2, text="Plot!", command = lambda: main(E1.get(),False)).pack(side='left')
 frame3 = Frame(root)
 frame3.pack()
-Button(frame3, text = "Save all", command = lambda: saveAll(E2.get())).pack(side = 'left')
-E2 = Entry(frame3, bd = 5)
+Button(frame3, text="Prev common", command = lambda: next_common(False)).pack(side='left')
+Button(frame3, text="Next common", command = lambda: next_common(True)).pack(side='left')
+Checkbutton(frame3, text="Save", variable = bool_save).pack(side='left')
+Checkbutton(frame3, text="Speed (~7 min per cell)", variable = boolSpeed).pack(side='left')
+frame4 = Frame(root)
+frame4.pack()
+Button(frame4, text = "Save all", command = lambda: saveAll(E2.get())).pack(side = 'left')
+Label(frame4, text = "Trashold: ").pack(side='left')
+E2 = Entry(frame4, textvariable = trashhold,bd = 5)
 E2.pack(side = 'left')
+w = OptionMenu(frame4, extention, ".png", ".svg")
+w.pack()
 
 # init figure
-fig = plt.figure(figsize=(8, 6), dpi=80)
+fig = plt.figure(dpi=80)
 canvas = FigureCanvasTkAgg(fig, root)
 toolbar = NavigationToolbar2TkAgg(canvas, root)
 canvas.get_tk_widget().pack()
